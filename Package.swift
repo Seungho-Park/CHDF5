5bcbfa30a4b293af5db1ec40f8ca29244aca648d
Seungho-Park <65594165+Seungho-Park@users.noreply.github.com>
2020-05-20 16:53:03 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,8 @@ import PackageDescription
 
 let package = Package(
     name: "CHDF5",
-    pkgConfig: "libhdf5"
+    pkgConfig: "libhdf5",
+    dependencies:[
+        .Package(url: "https://github.com/aleph7/CHDF5.git", majorVersion: 1)
+    ]
 )
